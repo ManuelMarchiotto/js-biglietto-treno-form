24d895e3129f30b4fa13764a27e2bca9761b4291
Manuel Marchiotto <manuelmarchiotto007@gmail.com>
2024-05-13 17:39:49 +0200

    add: main.js

---
+++ JS/main.js
@@ -0,0 +1,43 @@
+//chiedere all'utente il numero di chilometri che vuole percorrere
+const kilomiter = prompt("Quanti kilometri vuoi percorrere?");
+
+//chiedere all'utente l'età del passeggero
+const age = prompt("Quanti anni hai?");
+
+//Prezzo al kilometro
+const priceKm = 0.21;
+//Minorenni
+const underAge = 18;
+//Maggiorenni
+const overAge = 65;
+
+let discount;
+//Conversione delle stringhe in numeri
+Number.parseInt(kilomiter);
+Number.parseInt(age);
+
+//Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+//il prezzo del biglietto è definito in base ai km (0.21 € al km)
+//va applicato uno sconto del 20% per i minorenni
+//va applicato uno sconto del 40% per gli over 65.
+
+//Verifico che entrambi i dati convertiti siano numeri altrimenti blocco il tutto
+if(isNaN(kilomiter) || isNaN(age)){
+    console.log("Almeno uno dei dati messi non è un numero");
+//Visto che i dati sono entrambi dei numeri verifico l'età per lo sconto  
+}else if (age > underAge && age < overAge){
+    discount = 0;
+}else if(age <= underAge){
+    discount = 20;
+}else if(age >= overAge){
+    discount = 40;
+}
+
+//Calcolo di soldi da spendere per il viaggio
+
+const euroDiscount = ((kilomiter * priceKm)*discount)/100;
+const euro = (kilomiter * priceKm);
+
+
+document.getElementById("euro").innerHTML = `il biglietto costa ${euro.toFixed(2)}€`;
+console.log(euro.toFixed(2));
